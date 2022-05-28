95341c1b0bc9f0bfdbaaeb4b78a7716c934a523e
Paweł Sikora <1sikora.pawel@gmail.com>
2022-05-28 16:05:01 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./Search";
+import Dropdown from "./components/DropDown";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
-function App() {
+const items = [
+  {
+    title: `What is React?`,
+    content: `React is a front end js framework`,
+  },
+  {
+    title: `Why use React?`,
+    content: `React is a fav framework among engineeers`,
+  },
+  {
+    title: `How do u use React?`,
+    content: `You use React by creating components`,
+  },
+];
+
+const options = [
+  {
+    label: `The Color Red`,
+    value: `red`,
+  },
+  {
+    label: `The Color Green`,
+    value: `green`,
+  },
+  {
+    label: `A Shade of Blue`,
+    color: `blue`,
+  },
+];
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
-}
-
-export default App;
+};
